9d8affb66d3e5000ff12d51d8f41f015a17cdf40
dtun <dtun@me.com>
2024-03-18 06:15:26 -0700

    Add react-native-mmkv pkg

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { Button, Image, View } from 'react-native';
+import { Button, Image, View, Text } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { useMMKVString } from 'react-native-mmkv';
 
 function ImagePickerExample() {
   const [image, setImage] = useState<string | null>(null);
+  const [name, setName] = useMMKVString('user.name');
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -27,6 +29,8 @@ function ImagePickerExample() {
       {image && (
         <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
       )}
+      <Text>{name}</Text>
+      <Button title="Set name" onPress={() => setName('John Doe')} />
     </View>
   );
 }
