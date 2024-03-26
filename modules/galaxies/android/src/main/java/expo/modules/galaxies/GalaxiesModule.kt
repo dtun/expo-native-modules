d82fbf9f7a5e0eec6307569638c7fa5c48a3f739
dtun <dtun@me.com>
2024-03-26 09:05:22 -0700

    Fix Android module

--- modules/galaxies/android/src/main/java/expo/modules/galaxies/GalaxiesModule.kt
+++ modules/galaxies/android/src/main/java/expo/modules/galaxies/GalaxiesModule.kt
@@ -1,9 +1,17 @@
 package expo.modules.galaxies
 
+import android.content.pm.PackageInfo
+import android.os.Build
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class GalaxiesModule : Module() {
+  private val context
+    get() = requireNotNull(appContext.reactContext)
+
+  private fun getPackageInfo(): PackageInfo? {
+    return context.packageManager.getPackageInfo(context.packageName, 0);
+  }
 
   override fun definition() = ModuleDefinition {
 
